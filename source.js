e50e0bd2b3ea4eb2c65c6a051fcf4d27f370e527
lunchtray-chen <gtc477@gmail.com>
2025-12-16 21:52:53 -0600

    Fix fading bug

--- source.js
+++ source.js
@@ -1,3 +1,23 @@
+// code for making elements on website fade in
+
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries, obs) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show'); // trigger animation
+      obs.unobserve(entry.target); // stop watching after fade-in
+      const children = entry.target.querySelectorAll('img');
+      children.forEach((child, index) => {
+        child.style.transitionDelay = `${index * 0.3}s`;
+        child.classList.add('show');
+      });
+    }
+  });
+}, { threshold: 0.15 });
+
+faders.forEach(fadeEl => observer.observe(fadeEl));
+
 // makes the repetitive gallery html code
 
 const watchGallery = document.querySelector('.watch .gallery .content');
@@ -28,27 +48,10 @@ for (let i = 1; i <= watchImgDesc.length; i++) {
     </div>
     `
   );
+
+  const newEl = watchGallery.lastElementChild;
+  observer.observe(newEl); 
 }
-
-// code for making elements on website fade in
-
-const faders = document.querySelectorAll('.fade-in');
-
-const observer = new IntersectionObserver((entries, obs) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show'); // trigger animation
-      obs.unobserve(entry.target); // stop watching after fade-in
-      const children = entry.target.querySelectorAll('img');
-      children.forEach((child, index) => {
-        child.style.transitionDelay = `${index * 0.3}s`;
-        child.classList.add('show');
-      });
-    }
-  });
-}, { threshold: 0.15 });
-
-faders.forEach(fadeEl => observer.observe(fadeEl));
 
 // makes the sidebar appear and disappear when clicked
 
