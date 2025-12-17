c207985413ca5fef02b5beacaa80f8b82a300707
lunchtray-chen <gtc477@gmail.com>
2025-12-16 21:00:24 -0600

    Finalized watchtower page

--- source.js
+++ source.js
@@ -1,3 +1,37 @@
+// makes the repetitive gallery html code
+
+const watchGallery = document.querySelector('.watch .gallery .content');
+const watchImgDesc = [
+  "Watchtower's opening screen",
+  "Watchtower's settings screen",
+  "Initial UI mockup",
+  "Final UI design",
+  "Turnaround for Avery (deleted character)",
+  "Turnaround for Langley",
+  "Turnaround for Matthias (deleted character)",
+  "Turnaround for Samiha",
+  "Turnaround for Vera",
+  "Avery design iterations (deleted character)",
+  "Environment mockup",
+  "Beginning screen initial mockup"
+]
+
+for (let i = 1; i <= watchImgDesc.length; i++) {
+  watchGallery.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="fade-in">
+      <img 
+        src="images/watch-gallery/watch-${i}.webp"
+      >
+      <h3>${watchImgDesc[i - 1]}</h3>
+    </div>
+    `
+  );
+}
+
+// code for making elements on website fade in
+
 const faders = document.querySelectorAll('.fade-in');
 
 const observer = new IntersectionObserver((entries, obs) => {
@@ -12,18 +46,22 @@ const observer = new IntersectionObserver((entries, obs) => {
       });
     }
   });
-}, { threshold: 0.25 });
+}, { threshold: 0.15 });
 
 faders.forEach(fadeEl => observer.observe(fadeEl));
 
+// makes the sidebar appear and disappear when clicked
+
 function showSidebar(){
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "inline-block"
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "inline-block";
 }
+
 function hideSidebar(){
-  const sidebar = document.querySelector(".sidebar")
-  sidebar.style.display = "none"
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
+
 function showDropdown(){
   const dropdown = document.querySelector(".sidebar .dropdown")
   if (dropdown.style.display === "none" || dropdown.style.display === "") {
