1f02ed4ee8256c532adfcfcf3420a6eff443edb4
lunchtray-chen <gtc477@gmail.com>
2025-12-21 18:40:30 -0600

    Added image carousel to artwork subpage

--- source.js
+++ source.js
@@ -18,40 +18,21 @@ const observer = new IntersectionObserver((entries, obs) => {
 
 faders.forEach(fadeEl => observer.observe(fadeEl));
 
-// makes the repetitive gallery html code
 
-const watchGallery = document.querySelector('.watch .gallery .content');
-const watchImgDesc = [
-  "Watchtower's opening screen",
-  "Watchtower's settings screen",
-  "Initial UI mockup",
-  "Final UI design",
-  "Turnaround for Avery (deleted character)",
-  "Turnaround for Langley",
-  "Turnaround for Matthias (deleted character)",
-  "Turnaround for Samiha",
-  "Turnaround for Vera",
-  "Avery design iterations (deleted character)",
-  "Environment mockup",
-  "Beginning screen initial mockup"
-]
+// Code that makes the carousel work
 
-for (let i = 1; i <= watchImgDesc.length; i++) {
-  watchGallery.insertAdjacentHTML(
-    'beforeend',
-    `
-    <div class="fade-in">
-      <img 
-        src="images/watch-gallery/watch-${i}.webp"
-      >
-      <h3>${watchImgDesc[i - 1]}</h3>
-    </div>
-    `
-  );
+const nextButton = document.querySelector(".carousel-button");
 
-  const newEl = watchGallery.lastElementChild;
-  observer.observe(newEl); 
-}
+nextButton.addEventListener("click", () => {
+  const slides = nextButton.closest(".carousel").querySelector("[data-slides]")
+  const activeSlide = slides.querySelector("[data-active]")
+  let newIndex = [...slides.children].indexOf(activeSlide) + 1
+  if (newIndex >= slides.children.length) newIndex = 0
+
+  slides.children[newIndex].dataset.active = true
+  delete activeSlide.dataset.active
+}) 
+
 
 // makes the sidebar appear and disappear when clicked
 
@@ -73,3 +54,43 @@ function showDropdown(){
     dropdown.style.display = "none";
   }
 }
+
+
+// makes the repetitive gallery html code
+
+const watchGallery = document.querySelector('.watch .gallery .content');
+const watchImgDesc = [
+  "Watchtower's opening screen",
+  "Watchtower's settings screen",
+  "Initial UI mockup",
+  "Final UI design",
+  "Turnaround for Avery (deleted character)",
+  "Turnaround for Langley",
+  "Turnaround for Matthias (deleted character)",
+  "Turnaround for Samiha",
+  "Turnaround for Vera",
+  "Avery design iterations (deleted character)",
+  "Environment mockup",
+  "Beginning screen initial mockup"
+]
+
+function buildGallery() {
+  for (let i = 1; i <= watchImgDesc.length; i++) {
+    watchGallery.insertAdjacentHTML(
+      'beforeend',
+      `
+      <div class="fade-in">
+        <img 
+          src="images/watch-gallery/watch-${i}.webp"
+        >
+        <h3>${watchImgDesc[i - 1]}</h3>
+      </div>
+      `
+    );
+
+    const newEl = watchGallery.lastElementChild;
+    observer.observe(newEl); 
+  }
+}
+
+buildGallery();
