68b48503c263707707d53cb827cb61f9f0c091b2
lunchtray-chen <gtc477@gmail.com>
2026-03-17 20:10:50 -0700

    responsiveness second pass

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
     return (
         <div className='logo' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
             <img src="/logo-star.webp" className='logo-star'/>
-            <img src="/logo-text.webp" width={80} height={70}/>
+            <img src="/logo-text.webp" className='logo-text'/>
         </div>
     )
 }
